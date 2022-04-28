035b5414c477c7a1279d3a4c3dc320b3ae9d5436
cklicata <ckomroskylicata@uiowa.edu>
2022-04-27 19:31:00 -0500

    Test Commit

--- app.py
+++ app.py
@@ -5,16 +5,47 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pageTitle='Home')
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', pageTitle= "About")
 
 @app.route('/estimate')
 def estimate():
-    return render_template('estimate.html')
+    return return_template('estimate.html', pageTitle= "Estimating VTM")
+
+def calculate():
+    radius=request.form['radius']
+    height=request.form['height']
+    radius=float(radius)
+    height=float(height)
+    pi=3.14
+
+
+    tanktop_area=(radius*radius)*pi
+    tanksides_area=(2*(pi*(radius*height)))
+    total_area=tanksides_area+tanktop_area
+    area_sqft=total_area/144
+    material_cost=area_sqft*25
+    labor_cost=area_sqft*15
+    total_cost=material_cost+labor_cost
+    print("Total Cost Estimate: $")
     
+    return render_template('estimate.html',
+        total_cost=total_cost)
+    
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
